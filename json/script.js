1021793d7f3f6132501ed3b54ccfdda6d82eef9d
moorthi <moorthitechno@gmail.com>
2023-12-09 13:21:55 +0530

    new folder

--- json/script.js
+++ json/script.js
@@ -2,6 +2,13 @@
 console.log("connected !")
 //below mentioned are valid json formates
 //
+let timertable = document.createElement('div');
+let app = document.getElementById('app');
+app.appendChild(timertable);
+// timertable.style.width="400px";
+timertable.style.height="80px";
+timertable.style.backgroundColor="lightblue";
+app.style.backgroundColor="green"
 let json1 = "hello";
 let json2 = "a";
 let json3 = 4;
@@ -42,4 +49,10 @@ let json7 = `[
 let parsed = JSON.parse(json7);
 parsed.forEach(element => {
 console.log(element.name,"Working As ",element.pro)    
-})
+});
+var count = 0;
+let id = setInterval(() => {
+    count++;
+    timertable.innerHTML=count;
+    console.log("time out",count)
+}, 2000);
